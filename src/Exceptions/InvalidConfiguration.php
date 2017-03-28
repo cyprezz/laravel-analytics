44d72eb7654b206c7e8f8662002e880dd0a1eef4
Dmitry <cyprezz@gmail.com>
2017-03-28 15:10:20 +0300

    Revert "update base"

--- src/Exceptions/InvalidConfiguration.php
+++ src/Exceptions/InvalidConfiguration.php
@@ -8,7 +8,7 @@ class InvalidConfiguration extends Exception
 {
     public static function viewIdNotSpecified()
     {
-        return new static('There was no view id specified. You must provide a valid view id to execute querys on Google Analytics.');
+        return new static('You must provide a valid view id.');
     }
 
     public static function credentialsJsonDoesNotExist(string $path)
